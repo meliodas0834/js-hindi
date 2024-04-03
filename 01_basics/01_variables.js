2ebb763d0c32ed9fa0db7223ec406a18baaefa4d
meliodas0834 <144827567+meliodas0834@users.noreply.github.com>
2024-04-03 23:56:16 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 14456
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
