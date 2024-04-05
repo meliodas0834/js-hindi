92c23f2948e131020da5655537c0ae6fc7732b08
meliodas0834 <144827567+meliodas0834@users.noreply.github.com>
2024-04-05 20:31:09 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,5 +1,4 @@
 
-// console.log(2 > 1);
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 == 1);
@@ -17,6 +16,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// ==
 
 console.log("2" === 2);
